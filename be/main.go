4bc573d3b22d5081c0f977a1fe94dd866ece9f67
alyx <alyx@malkier.net>
2022-04-05 23:43:05 -0500

    Add IP endpoint for IP address debugging

--- be/main.go
+++ be/main.go
@@ -64,9 +64,16 @@ func main() {
 	r := gin.Default()
 	r.ForwardedByClientIP = true
 	r.SetTrustedProxies([]string{"0.0.0.0/0"})
+
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
+		})
+	})
+
+	r.GET("/ip", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"ip": c.ClientIP(),
 		})
 	})
 
